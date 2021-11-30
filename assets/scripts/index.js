8f4e054d1fce6374169dd3439dffeb922a0c485c
Silvia <silvia.pabon.2020@upb.edu.co>
2021-11-29 23:52:40 -0500

    Anexos en index.js
    -Se anexa código que permite que se pueda expandir o esconder la navbar de forma vertical, luego de dar clic en el botón hamburguesa (cuando se está en una resolución móvil)
    -Se anexa código que facilita la accesibilidad en el botón de hamburguesa agregando un aria-label indicando "abrir menú" "cerrar menú".
    -Pendiente anexar mejores comentarios.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,18 @@
-console.log('Nada por aquí'); 
+//Constantes
+const navToggle = document.querySelector("nav .wrap-menu")
+const navMenu = document.querySelector(".nav__navigation")
+const body = document.getElementsByTagName("body")[0]
+
+//Pendiente comentario
+navToggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav__navigation_visible");
+
+    body.classList.toggle("body__fixed");
+
+    if (navMenu.classList.contains("nav__navigation_visible")){
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menú");
+    }
+});
+
